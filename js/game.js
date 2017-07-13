61da537c7df3ff03ce63c4294138e278a28c00a1
rmeaney <rjmeaney@gmail.com>
2017-07-13 04:21:45 -0400

    added error correction

--- js/game.js
+++ js/game.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
         $('#courtesyLink').html('');
         var url = $('#userUrl').val().trim();
          //http://www.youtube.com/watch?v=SNfYz6Yw0W8&feature=g-all-esi would work also
+         if(url.match('v=')){
         var a = url.split("v=")[1];
 
         //define start Time input variables
@@ -36,6 +37,7 @@ $(document).ready(function(){
 
         a = a != undefined ? a : url.split("youtu.be/")[1];
         b = a.split("&")[0];
+
         finalLink = 'https://www.youtube.com/embed/' + a + '?start=' + userStartTime +'&end='+userEndTime+'&version=3';
         $('#outputLink').append(finalLink+'<br/>');
         $('#courtesyLink').append("<a href = " + finalLink + '>' + "Click Here</a>");
@@ -61,6 +63,10 @@ $(document).ready(function(){
 
         //clear userURL field for next use
         $('#userUrl').val('');
+    }else{
+        modal('not a valid url');
+    }
 
 });
+
 });
